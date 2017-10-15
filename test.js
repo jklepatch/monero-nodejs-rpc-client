8d65f621c6f182d759f87333c0d379bb745a2c41
Julien Klepatch <julien@julienklepatch.com>
2017-10-15 17:59:49 +0800

    Fixed wrong way of calling rpcClient constructor in tests.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const rpcClass = require('monero-rpc-client');
 const WALLET_ADDRESS = '46sKRpY9ULrhA7QVWxZ1VPWmTeU56CBfUNeCey5xDRsBMXnxP5fyBvYWYTH5xEnPuUJtiHcJNHJZ9fgsfkxbAS2zVhS7DBm';
 const NODE_ADDRESS = 'http://node.moneroworld.com:18089';
 
-const rpc = new rpcClass(NODE_ADDRESS, WALLET_ADDRESS);
+const rpc = new rpcClass(NODE_ADDRESS, true);
 
 const chai = require('chai');
 const expect = chai.expect;
